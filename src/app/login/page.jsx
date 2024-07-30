83ba8f721fe4babb2956834b86d93da7306c7e2a
Achala-Nayak <achala.nayak2@gmail.com>
2024-07-31 00:06:57 +0530

    write blog- Achala Nayak

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+"use client";
+import { signIn, useSession } from "next-auth/react";
+import styles from "./loginPage.module.css";
+import { useRouter } from "next/navigation";
 
-const LoginPage = () => {
+const LoginPage = () => {  
   return (
-    <div>LoginPage</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <div className={styles.socialButton} onClick={() => signIn("google")}>
+          Sign in with Google
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default LoginPage
+export default LoginPage;
