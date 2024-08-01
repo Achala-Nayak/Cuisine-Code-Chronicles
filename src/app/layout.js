609e560717083ea5064bfe5bc1e64b271954f7c1
Achala-Nayak <achala.nayak2@gmail.com>
2024-08-01 22:03:46 +0530

    Frontend- Achala Nayak

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import Navbar from "@/components/navbar/Navbar";
 import Footer from "@/components/footer/Footer";
 import { ThemeContextProvider } from '@/context/ThemeContext';
 import ThemeProvider from '@/providers/ThemeProvider';
-import AuthProvider from '@/providers/AuthProvider';
+// import AuthProvider from '@/providers/AuthProvider';
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -18,7 +18,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <AuthProvider>
+        {/* <AuthProvider> */}
         <ThemeContextProvider>
           <ThemeProvider>
         <div className='container'>
@@ -30,7 +30,7 @@ export default function RootLayout({ children }) {
         </div>
         </ThemeProvider>
         </ThemeContextProvider>
-        </AuthProvider>
+        {/* </AuthProvider> */}
       </body>
     </html>
   )
