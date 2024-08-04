715872f35caccbbf27fffcdc50913120a40a95e6
Achala-Nayak <achala.nayak2@gmail.com>
2024-08-04 22:42:31 +0530

    update

--- src/components/categoryList/CategoryList.jsx
+++ src/components/categoryList/CategoryList.jsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 import Image from "next/image";
 
 const getData = async () => {
-  // const res = await fetch("http://localhost:3000/api/categories", {
-  const res = await fetch("https://cuisine-code-chronicles.vercel.app/api/categories", {
+  const res = await fetch("http://localhost:3000/api/categories", {
+  // const res = await fetch("https://cuisine-code-chronicles.vercel.app/api/categories", {
     cache: "no-store",
   });
 
--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -6,8 +6,8 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 const fetchPostData = async (postSlug) => {
-  // const res = await fetch(`http://localhost:3000/api/posts/${postSlug}`, {
-    const res = await fetch(` https://cuisine-code-chronicles.vercel.app/api/posts/${postSlug}`, {
+  const res = await fetch(`http://localhost:3000/api/posts/${postSlug}`, {
+    // const res = await fetch(` https://cuisine-code-chronicles.vercel.app/api/posts/${postSlug}`, {
    
     cache: "no-store",
   });
